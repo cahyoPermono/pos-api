a8755e2aae9fdda0b357d094344ad18b8afd11d9
Cahyo <cahyo.adi@imaniprima.com>
2020-09-10 09:10:06 +0700

    Adding knex and pg, setting connection db

--- server.js
+++ server.js
@@ -2,9 +2,28 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res)=> {
-    res.send("Hello");
+const knex = require("knex");
+
+const postgres = knex({
+  client: "pg",
+  connection: {
+    host: "127.0.0.1",
+    user: "widodo",
+    password: "widodo",
+    database: "pos-widodo",
+  },
 });
+
+console.log(postgres.select('*').from('users'));
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+/*
+    /signin --> POST = Succses/fail
+    /register --> POST = User
+*/
 
 app.listen("3000", () => {
   console.log("Your Server Started in port 3000");
