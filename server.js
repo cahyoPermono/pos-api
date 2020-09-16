f2ca4bc87395ce5597044fffc2086997f209133b
Cahyo <cahyo.adi@imaniprima.com>
2020-09-16 11:03:48 +0700

    adding new migrations, adding post register

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 //import crypto for password
 const bcrypt = require("bcryptjs");
@@ -11,14 +12,14 @@ const knexConf = require("./knexfile");
 
 const app = express();
 
-let tempHash= "";
+let tempHash = "";
 
 //middleware
 app.use(bodyParser.json());
+app.use(cors());
 
 //initialize knex
 const knex = Knex(knexConf.development);
-
 
 app.get("/", (req, res) => {
   res.send("Hello");
@@ -26,16 +27,33 @@ app.get("/", (req, res) => {
 
 //sign in route
 app.post("/signin", (req, res) => {
-
+  knex
+    .select("*")
+    .from("users")
+    .then((data) => {
+      console.log(data);
+    });
   res.json(bcrypt.compareSync(req.body.password, tempHash));
 });
 
 //register route
 app.post("/register", (req, res) => {
+  //getting the needed data for store in db
+  const { username, password } = req.body;
+
   //encrypt the password
-  let hash = bcrypt.hashSync(req.body.password, saltRounds);
+  let hash = bcrypt.hashSync(password, saltRounds);
   tempHash = hash;
-  res.json(hash);
+
+  //insert to user
+  knex("users")
+    .returning(["id", "username"])
+    .insert({
+      username: username,
+      password: hash,
+    })
+    .then((data) => res.json(data))
+    .catch((e) => res.status(400).json(e));
 });
 
 /*
